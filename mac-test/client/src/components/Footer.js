5038afbac0d97efbba68a82bb3930d81bba68410
pedro <pedrowada2004@gmail.com>
2024-01-15 11:59:15 +1000

    Details

--- mac-test/client/src/components/Footer.js
+++ mac-test/client/src/components/Footer.js
@@ -1,15 +1,14 @@
-import {FaFacebook, FaLinkedinIn, FaInstagram, FaCopyright} from 'react-icons/fa'
+import {FaLinkedinIn, FaCopyright, FaGithub} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 import styles from './styles/Footer.module.css'
 
 export default function Footer() {
     return(
         <div className={styles.container}>
-            <p>Follow us in our social medias:</p>
+            <p>Follow me in my social medias:</p>
             <div>
-                <Link to='/' target='_blank'><FaFacebook/></Link>
-                <Link to='/' target='_blank'><FaInstagram/></Link>
-                <Link to='/' target='_blank'><FaLinkedinIn/></Link>
+                <Link to='https://github.com/PedroWada' target='_blank'><FaGithub/></Link>
+                <Link to='https://www.linkedin.com/in/pedro-wada-11a2452a2/' target='_blank'><FaLinkedinIn/></Link>
                 <FaCopyright/>
             </div>
         </div>
